863078bc5a1b89402c834c5b00477369cd19e523
hendisantika <hendisantika@yahoo.co.id>
2023-09-26 17:48:02 +0700

    #6 - Add CustomerController ✏️📗:octocat:⬆

--- src/main/java/com/hendisantika/client/ClientApplication.java
+++ src/main/java/com/hendisantika/client/ClientApplication.java
@@ -1,8 +1,13 @@
 package com.hendisantika.client;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.support.WebClientAdapter;
 import org.springframework.web.service.annotation.GetExchange;
@@ -36,4 +41,22 @@ interface CustomerHttpClient {
 }
 
 record Customer(int id, String name) {
+}
+
+@Controller
+@ResponseBody
+class CustomerController {
+
+	private final CustomerHttpClient client;
+	Logger log = LoggerFactory.getLogger(CustomerController.class);
+
+	CustomerController(CustomerHttpClient client) {
+		this.client = client;
+	}
+
+	@GetMapping("/all")
+	public Flux<Customer> all() {
+		log.info("Fetching customers");
+		return client.fetchCustomers();
+	}
 }
